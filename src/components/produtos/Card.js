f0e351b77ea8136dfae9d9be08604ec9044134a4
Helaine <helainecrcosta@gmail.com>
2022-02-17 20:36:26 -0300

    Alteração na pasta img e na pasta produtos/card.js

--- src/components/produtos/Card.js
+++ src/components/produtos/Card.js
@@ -4,12 +4,14 @@ import styled from "styled-components"
 const CardsContainer = styled.div`
   display: flex;
   flex-direction: column;
+ 
 
   img {
     display: flex;
-    width: 40%;
-    margin-top: 10px;
-    margin-bottom: 30px;
+    width: 50%;
+    margin: 0 auto; 
+    align-items: center;
+   
   }
 `
 
@@ -18,11 +20,12 @@ const CardInfos = styled.div`
    flex-direction: column;
    justify-content: center;
    align-items: center;
-
 `
 
 const AddCarrinho = styled.button`
   align-self: center;
+  margin-bottom: 60px;
+  
 `
 
 class Card extends React.Component {
